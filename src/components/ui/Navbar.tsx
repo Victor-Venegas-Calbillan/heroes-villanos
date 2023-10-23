b12b62ef21769e6309e2247cc1ba0bd9cf974359
Victor Venegas <victor.venegas.call@gmail.com>
2023-10-23 13:25:40 -0300

    Finalizando navbar responsive

--- src/components/ui/Navbar.tsx
+++ src/components/ui/Navbar.tsx
@@ -1,14 +1,32 @@
+import { useState } from "react";
+
 export const Navbar = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const handleToggle = () => {
+    setIsOpen(!isOpen);
+  };
   return (
     <nav className="navbar">
       <div className="navbar__container">
         <h1 className="navbar__logo">Logo</h1>
-        <ul className="navbar__menu">
+        <ul
+          className={`navbar__menu ${
+            isOpen ? "navbar__menu--active" : ""
+          }`}
+        >
           <li className="navbar__item">Characters</li>
-          <li className="navbar__item">Comics</li>
           <li className="navbar__item">Marvel</li>
           <li className="navbar__item">Dc</li>
         </ul>
+
+        <div className="navbar__toggle" onClick={handleToggle}>
+          <i
+            className={`${
+              isOpen ? "fas fa-times" : "fas fa-bars"
+            } navbar__hamburger`}
+          ></i>
+        </div>
       </div>
     </nav>
   );
