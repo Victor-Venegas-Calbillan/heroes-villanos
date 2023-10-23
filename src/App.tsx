99bfd3a13f9cca98604cfe1a7f1d3cd69a638d56
Victor Venegas <victor.venegas.call@gmail.com>
2023-10-23 02:22:37 -0300

    Avanzando en el proyecto

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import { Navbar } from "./components/ui/Navbar";
+
+function App() {
+  return (
+    <>
+      <Navbar />
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/ui/Navbar.tsx
@@ -0,0 +1,15 @@
+export const Navbar = () => {
+  return (
+    <nav className="navbar">
+      <div className="navbar__container">
+        <h1 className="navbar__logo">Logo</h1>
+        <ul className="navbar__menu">
+          <li className="navbar__item">Characters</li>
+          <li className="navbar__item">Comics</li>
+          <li className="navbar__item">Marvel</li>
+          <li className="navbar__item">Dc</li>
+        </ul>
+      </div>
+    </nav>
+  );
+};
